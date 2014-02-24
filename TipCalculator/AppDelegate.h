aaf27a1bf3488d09c4cc5963d204b0571e10989f
Eric Socolofsky <eric@transmote.com>
2014-02-23 16:51:40 -0800

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Eric Socolofsky on 2/23/14.
+//  Copyright (c) 2014 Eric Socolofsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
